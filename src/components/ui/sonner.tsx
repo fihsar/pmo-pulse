ae11d4eb6c10447d35a3c2b7f258737db30e7de8
Fihsar Hafiz <fihsarhafiz@gmail.com>
2026-05-30 11:44:10 +0700

    feat: add reusable ui components

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Toaster as Sonner } from 'sonner';
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+function Toaster({ ...props }: ToasterProps) {
+  return <Sonner className="toaster group" toastOptions={{ classNames: { toast: 'group toast group-[.toaster]:bg-white group-[.toaster]:text-slate-950 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg' } }} {...props} />;
+}
+
+export { Toaster };
